04cf2a371bf48687f872d91df466f36fd8f5f1b1
davidfrp <davidfrp@outlook.dk>
2021-06-08 14:12:11 +0200

    Added missing @Repository annotation for user repository.

--- src/main/java/com/davidfrp/wishlisting/repository/UserRepository.java
+++ src/main/java/com/davidfrp/wishlisting/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.davidfrp.wishlisting.repository;
 
 import com.davidfrp.wishlisting.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
