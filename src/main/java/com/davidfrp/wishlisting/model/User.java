5eb73d32223ea19542e79d960f105645515a39b2
davidfrp <davidfrp@outlook.dk>
2021-06-08 14:08:27 +0200

    Added a user model object.

---
+++ src/main/java/com/davidfrp/wishlisting/model/User.java
@@ -0,0 +1,50 @@
+package com.davidfrp.wishlisting.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "username_uindex", columnNames = "username")
+        }
+)
+public class User {
+
+    @Id
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "display_name")
+    private String displayName;
+
+    @Column(name = "password")
+    private String password;
+
+    protected User() { }
+
+    public User(String username, String displayName, String password) {
+        this.username = username;
+        this.displayName = displayName;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
