a89b5f3a5725536f77885a7e0030da02f3ade582
davidfrp <davidfrp@outlook.dk>
2021-06-08 13:36:48 +0200

    Initial commit

---
+++ src/main/java/com/davidfrp/wishlisting/WishlistingApplication.java
@@ -0,0 +1,13 @@
+package com.davidfrp.wishlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davidfrp/wishlisting/WishlistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidfrp.wishlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
