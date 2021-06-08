a3886b3bc569f663f7178096ecd364cac7d4c6cb
davidfrp <davidfrp@outlook.dk>
2021-06-08 17:24:34 +0200

    Added some business logic methods.

--- src/main/java/com/davidfrp/wishlisting/service/UserService.java
+++ src/main/java/com/davidfrp/wishlisting/service/UserService.java
@@ -1,5 +1,6 @@
 package com.davidfrp.wishlisting.service;
 
+import com.davidfrp.wishlisting.model.User;
 import com.davidfrp.wishlisting.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,21 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User registerUser(String displayName, String username, String password) {
+        return userRepository.save(new User(displayName, username, password));
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userRepository.findByUsername(username) != null;
+    }
+
+    public boolean doesUsernameAndPasswordMatch(String username, String password) {
+        return userRepository.findByUsernameAndPassword(username, password) != null;
     }
 }
