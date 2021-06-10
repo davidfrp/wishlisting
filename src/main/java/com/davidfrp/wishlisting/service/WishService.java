fc7882153961a05cb8789b300b0f9ea34db69b6a
davidfrp <davidfrp@outlook.dk>
2021-06-10 10:29:46 +0200

    Added method for creating wishes.

--- src/main/java/com/davidfrp/wishlisting/service/WishService.java
+++ src/main/java/com/davidfrp/wishlisting/service/WishService.java
@@ -1,5 +1,6 @@
 package com.davidfrp.wishlisting.service;
 
+import com.davidfrp.wishlisting.model.Wish;
 import com.davidfrp.wishlisting.repository.WishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,9 @@ public class WishService {
     @Autowired
     public WishService(WishRepository wishRepository) {
         this.wishRepository = wishRepository;
+    }
+
+    public Wish createWish(Wish wish) {
+        return wishRepository.save(wish);
     }
 }
